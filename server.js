62f42674240d8801718d324510c69287bb70c6c1
Dmitry I <dmitry5768@yandex.ru>
2023-08-11 01:04:56 +0300

    Убрал лишнее оставил только раздачу статики

--- server.js
+++ server.js
@@ -1,40 +1,13 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import express from 'express';
-import { engine } from "express-handlebars";
-import handlebars from 'handlebars';
-
-// const handlebars = require('handlebars');
-
-import {resolve} from 'path'
-/*import { defineConfig } from 'vite'
-import handlebars from 'vite-plugin-handlebars';
-
-import card from "./src/layouts/card/card";
-
-import profile from "./src/layouts/profile-container/profile-container";
-import avatarChange from "./src/layouts/avatar-change/avatar-card"; */
-
-// console.log(handlebars)
-
-// Компилируем шаблон
-
+// import { engine } from "express-handlebars";
 
 const app = express();
 const PORT = 3000;
 
 const currentDir = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(currentDir);
-
-app.engine("handlebars", engine({
-    extname: ".hbs",
-    defaultLayout: "src/partials/layouts/page",
-    layoutsDir: path.resolve(__dirname, "src/layouts"),
-    partialsDir: path.resolve(__dirname, "src/partials"),
-  }));
-
-  app.set("view engine", "handlebars");
-app.set("views", path.resolve(__dirname, "src"));
 
 app.use(express.static(__dirname + '/'));
 
