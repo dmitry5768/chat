6481dbc59376b242cd2e4960d1bf98d3791445ac
Dmitry I <dmitry5768@yandex.ru>
2023-08-10 03:28:25 +0300

    added server.js

--- index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const root = document.querySelector('#app')
-    const result = greetings({username: 'John Deer'})
-    root.innerHTML = result
-})
